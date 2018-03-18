edb57ef7bd53f46e3f337d0e671455381e70f314
fcschmidt <fcschimidt@gmail.com>
2018-03-18 13:16:46 -0300

    Add python install

---
+++ python.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+#
+#  Install Python and Config Setup
+#
+sudo apt-get install python3-pip
+pip install -U pip
+pip3 install --upgrade pip
+#
+# Install Virtualenv
+pip3 install virtualenv
+#
+# Install Jupyter
+pip3 install jupyter
+pip3 install -U jupyter

a8f8afc9fd07b0d51b10edbf154a9f4793c66155
fcschmidt <fcschimidt@gmail.com>
2018-04-04 20:36:29 -0300

    Add haskell and GEO install

---
+++ haskell.sh
@@ -0,0 +1,13 @@
+#/bin/bash
+# Install Haskell
+
+sudo apt-get update
+sudo apt-get install -y software-properties-common
+sudo add-apt-repository -y ppa:hvr/ghc
+sudo apt-get update
+sudo apt-get install -y cabal-install-1.22 ghc-7.10.3
+cat >> ~/.bashrc <<EOF
+export PATH="\$HOME/.cabal/bin:/opt/cabal/1.22/bin:/opt/ghc/7.10.3/bin:\$PATH"
+EOF
+export PATH=~/.cabal/bin:/opt/cabal/1.22/bin:/opt/ghc/7.10.3/bin:$PATH
+

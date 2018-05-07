7afae640b123b2a609ae9bc45365917935570140
fcschmidt <fcschimidt@gmail.com>
2018-05-06 21:35:54 -0300

    Create new installer

---
+++ install_startUML.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+#  Install StarUML
+#  http://staruml.io/
+#
+wget http://staruml.io/download/release/v2.8.1/StarUML-v2.8.1-64-bit.deb
+dpkg StarUML-v2.8.1-64-bit.deb
+#
+apt-get update

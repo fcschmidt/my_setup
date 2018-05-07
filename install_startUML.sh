3a676ef972bf690a9094dfe2d2eda2656275166a
fcschmidt <fcschimidt@gmail.com>
2018-05-06 21:52:22 -0300

    Delete item

--- install_startUML.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-#  Install StarUML
-#  http://staruml.io/
-#
-wget http://staruml.io/download/release/v2.8.1/StarUML-v2.8.1-64-bit.deb
-dpkg -i StarUML-v2.8.1-64-bit.deb
-#
-apt-get update

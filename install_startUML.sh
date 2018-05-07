64efb9c8e3d2c297ea6909ea2b44b862e079062b
fcschmidt <fcschimidt@gmail.com>
2018-05-06 21:50:11 -0300

    Update

--- install_startUML.sh
+++ install_startUML.sh
@@ -3,6 +3,6 @@
 #  http://staruml.io/
 #
 wget http://staruml.io/download/release/v2.8.1/StarUML-v2.8.1-64-bit.deb
-dpkg StarUML-v2.8.1-64-bit.deb
+dpkg -i StarUML-v2.8.1-64-bit.deb
 #
 apt-get update
